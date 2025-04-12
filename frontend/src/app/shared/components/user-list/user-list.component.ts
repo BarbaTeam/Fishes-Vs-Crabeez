b42b386337808618b0a90046099b46bf6c659230
jm <d@onidra.net>
2025-04-12 21:13:17 +0200

    chore(ui): updated ergo side with user list (#63)

--- frontend/src/app/shared/components/user-list/user-list.component.ts
+++ frontend/src/app/shared/components/user-list/user-list.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service'
+import { ChildListPageComponent } from 'src/app/pages/child/list-page/child-list-page.component';
+import { ErgoConfigPageComponent } from 'src/app/pages/ergo/config-page/ergo-config-page.component';
+import { ErgoStatPageComponent } from 'src/app/pages/ergo/stat-page/ergo-stat-page.component';
 
 @Component({
   selector: 'app-user-list',
@@ -12,17 +16,25 @@ import { UserService } from '../../services/user.service'
 export class UserListComponent {
     public userList: User[] = [];
 
-    constructor(private userService: UserService,  private router: Router){
+    constructor(private userService: UserService,  private router: Router, private route: ActivatedRoute){
         this.userService.users$.subscribe((users: User[]) => {
             this.userList = users;
         });
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        console.log(this.route);
+    }
 
     selectUser(userId:User['userId']): void {
         this.userService.selectUser(userId);
-        this.router.navigate(['/child-play']);
+        if(this.route.component!.name == "ChildListPageComponent")
+            this.router.navigate(['/child-play']);
+        if(this.route.component!.name == "ErgoConfigPageComponent")
+            this.router.navigate(['/ergo-config-selected']);
+        if(this.route.component!.name == "ErgoStatPageComponent")
+            this.router.navigate(['/ergo-stat-selected']);
+        
     }
 }
 
