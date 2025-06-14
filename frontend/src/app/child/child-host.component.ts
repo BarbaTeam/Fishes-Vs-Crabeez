98841fbc2ac95d9f23ab5ae7b9145584c1acf43b
Jean Mi <d@onidra.net>
2025-06-14 21:54:18 +0200

    back-feat(socket): added forced disconnection from ergo to user

--- frontend/src/app/child/child-host.component.ts
+++ frontend/src/app/child/child-host.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocketService } from '@app/shared/services/socket.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
   selector: 'app-child-host',
   template: `<router-outlet></router-outlet>`,
 })
-export class ChildHostComponent {}
+export class ChildHostComponent implements OnInit, OnDestroy{
+    private subscriptions = new Subscription();
+
+    constructor(
+        private socket : SocketService,
+        private router : Router,
+    ){}
+
+    ngOnInit(): void {
+        this.subscriptions.add(
+            this.socket.on<void>('goBackHome').subscribe(()=>{
+                this.router.navigate(['/home']);
+            })
+        )
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe();
+    }
+
+}
