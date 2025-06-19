aac81046fdc296193f1b5f995939ca35c72ff604
PatBiz <patrick.bizot@etu.univ-cotedazur.fr>
2025-06-19 11:58:15 +0200

    wip(e2e): adding last scenarion - ergo change config of game lobby and child watch changes

---
+++ frontend/e2e/scenarios/ergo-change-lobby.spect.ts
@@ -0,0 +1,33 @@
+import { test, expect, chromium, Page} from '@playwright/test';
+import { testUrl } from 'e2e/e2e.config';
+
+
+
+test.describe('...', () => {
+    test.setTimeout(120_000);
+
+    let childPage: Page;
+    let ergoPage: Page;
+
+    test.beforeAll(async () => {
+        const browser = await chromium.launch();
+        const ergoContext = await browser.newContext();
+        const childContext = await browser.newContext();
+
+        ergoPage = await ergoContext.newPage();
+        await ergoPage.goto(testUrl);
+
+        childPage = await childContext.newPage();
+        await childPage.goto(testUrl);
+    });
+
+    // TODO : Declaring fixture :
+
+    test.beforeEach(async ({ page }) => {
+        // TODO : Initializing fixtures
+    });
+
+    test('...', async () => {
+        // TODO : Defining last test :crossed-fingers:
+    });
+});
