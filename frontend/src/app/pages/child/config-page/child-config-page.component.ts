1709b5789707c0df6859e21426f932be2c2de465
PatBiz <patrick.bizot@etu.univ-cotedazur.fr>
2025-06-24 00:02:35 +0200

    front-fix(ui): fixed leaderboard visibility toggle (#65)

--- frontend/src/app/pages/child/config-page/child-config-page.component.ts
+++ frontend/src/app/pages/child/config-page/child-config-page.component.ts
@@ -3,17 +3,21 @@ import { UserService } from 'src/app/shared/services/user.service';
 import { User } from 'src/app/shared/models/user.model';
 import { Router } from '@angular/router';
 
+
+
 @Component({
-  selector: 'app-child-config-page',
-  templateUrl: './child-config-page.component.html',
-  styleUrl: './child-config-page.component.scss'
+    selector: 'app-child-config-page',
+    templateUrl: './child-config-page.component.html',
+    styleUrl: './child-config-page.component.scss'
 })
 export class ChildConfigPageComponent {
-
     user!: User;
     userTemp!: User;
-    
-    constructor( private userService: UserService, private router: Router) {}
+
+    constructor(
+        private userService: UserService,
+        private router: Router
+    ) {}
 
     ngOnInit(): void {
         this.userService.selectedUser$.subscribe((user: User) => {
@@ -21,11 +25,11 @@ export class ChildConfigPageComponent {
         });
     }
 
-    onUserTempChanged(updatedUser: User): void {
+    public onUserTempChanged(updatedUser: User): void {
         this.userTemp = updatedUser;
     }
 
-    saveChanges(): void {
+    public saveChanges(): void {
         if (this.userTemp) {
             this.userService.saveChanges(this.userTemp);
         }
